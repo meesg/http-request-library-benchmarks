3cd3e92d881a7ed5e2aed8aa2b1b5edc566e9c3a
Mees Gribnau <m.gribnau@student.tudelft.nl>
2020-03-05 15:50:00 +0100

    Add C# HttpClient benchmarks

--- README.md
+++ README.md
@@ -1,9 +1,16 @@
 # http-request-library-benchmarks
+  
+  
+## Notes
 
+In the node code, the synchronous functions should also be await-ed to make it a little bit more fair.
+ 
 ## results
 
-language: node  
-library: got  
+### node
+
+#### got
+
 website: https://httpbin.org/get  
 location: university library  
 time: 2020-03-05T11:28:00+01:00  
@@ -14,8 +21,6 @@ time: 2020-03-05T11:28:00+01:00
 	Average response time over 100 synchronous request: 442.36  
 	Average response time over 100 asynchronous request: 309.94  
   
-language: node  
-library: got  
 website: https://httpbin.org/get  
 location: university library  
 time: 2020-03-05T11:39:00+01:00  
@@ -26,8 +31,7 @@ time: 2020-03-05T11:39:00+01:00
 	Average response time over 100 synchronous request: 429.18  
 	Average response time over 100 asynchronous request: 329.35  
   
-language: node  
-library: superagent  
+#### superagent
 website: https://httpbin.org/get  
 location: university library  
 time: 2020-03-05T11:39:00+01:00  
@@ -37,3 +41,16 @@ time: 2020-03-05T11:39:00+01:00
 	Average response time over 10 asynchronous request: 422.6  
 	Average response time over 100 synchronous request: 678.37  
 	Average response time over 100 asynchronous request: 428.31  
+
+### C-sharp
+
+#### HttpClient 
+website: https://httpbin.org/get  
+location: university library  
+time: 2020-03-05T15:30:00+01:00  
+	Average response time over 1 synchronous request:400  
+	Average response time over 1 asynchronous request:189  
+	Average response time over 10 synchronous request:182  
+	Average response time over 10 asynchronous request:100  
+	Average response time over 100 synchronous request:235  
+	Average response time over 100 asynchronous request:119
---
+++ c#/HttpClient/HttpClient/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Diagnostics;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace HttpClientBenchmark
+{
+    class Program
+    {
+        static readonly HttpClient client = new HttpClient();
+
+        static async Task Main(string[] args)
+        {
+            await synchAverage(1);
+            await asynchAverage(1);
+            await synchAverage(10);
+            await asynchAverage(10);
+            await synchAverage(100);
+            await asynchAverage(100);
+        }
+
+        static async Task<int> SingleRequest()
+        {
+            // Call asynchronous network methods in a try/catch block to handle exceptions.
+            try
+            {
+                Stopwatch stopWatch = new Stopwatch();
+                stopWatch.Start();
+                HttpResponseMessage response = await client.GetAsync("http://www.httpbin.org/get");
+                stopWatch.Stop();
+
+                return (int)stopWatch.ElapsedMilliseconds;
+            }
+            catch (HttpRequestException e)
+            {
+                Console.WriteLine("\nException Caught!");
+                Console.WriteLine("Message :{0} ", e.Message);
+
+                return -1;
+            }
+        }
+
+        static async Task<int> synchAverage(int n)
+        {
+            Task<int>[] tasks = new Task<int>[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                tasks[i] = SingleRequest();
+            }
+
+            int total = 0;
+            await Task.WhenAll(tasks);
+
+            for (int i = 0; i < n; i++)
+            {
+                total += tasks[i].Result;
+            }
+
+            int average = total / n;
+
+            Console.WriteLine("Average response time over " + n + " synchronous request:" + average);
+
+            return average;
+        }
+
+        static async Task asynchAverage(int n)
+        {
+            Task<int>[] tasks = new Task<int>[n];
+
+            int total = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                tasks[i] = SingleRequest();
+                total += await SingleRequest();
+            }
+
+            int average = total / n;
+
+            Console.WriteLine("Average response time over " + n + " asynchronous request:" + average);
+        }
+    }
+}
